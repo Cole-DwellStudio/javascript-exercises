935e396adf01da551bedd7b2ec8eb566a6e0ecb6
Cole Harris <cole@dwellstudioco.com>
2022-12-18 00:28:22 -0700

    Solve 12 findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const peopleOrdered = people.sort((a, b) => {
+    aYear = !a.yearOfDeath ? (year = new Date().getFullYear()) : a.yearOfDeath;
+    bYear = !b.yearOfDeath ? (year = new Date().getFullYear()) : b.yearOfDeath;
+    return aYear - a.yearOfBirth > bYear - b.yearOfBirth ? -1 : 1;
+  });
 
+  return peopleOrdered[0];
 };
 
 // Do not edit below this line
