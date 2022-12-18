5276fd5dc293e48708855fd67236f6b14a8026b2
Cole Harris <cole@dwellstudioco.com>
2022-12-17 18:26:22 -0700

    Solve 09 palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,29 @@
-const palindromes = function () {
+const palindromes = function (testString) {
+  // strip out all non-letter characters
+  // convert to all lower-case
+  // make a reversed copy of the string
+  // compare the reversed copy to the original
+  // if they are identical, return true
+  // else return false
 
+  // convert to lowercase
+  testString = testString.toLowerCase();
+
+  // remove all special characters
+  testString = testString.replaceAll("!", "");
+  testString = testString.replaceAll(".", "");
+  testString = testString.replaceAll(",", "");
+  testString = testString.replaceAll(" ", "");
+
+  let reversedStringArray = [];
+  for (let i = 0; i <= testString.length; i++) {
+    reversedStringArray.unshift(testString[i]);
+  }
+
+  reversedString = reversedStringArray.toString();
+  reversedString = reversedString.replaceAll(",", "");
+
+  return reversedString === testString;
 };
 
 // Do not edit below this line
