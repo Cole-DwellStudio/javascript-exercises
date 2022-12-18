e9913f7617e392fb3999239d15eec449a4475728
Cole Harris <cole@dwellstudioco.com>
2022-12-17 23:30:26 -0700

    Solve 10 fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (nthNumber) {
+  if (nthNumber.toString().includes("-")) {
+    return "OOPS";
+  }
 
+  nthNumber = parseInt(nthNumber);
+
+  let fibonacciSequence = 1;
+  let CurrentNum = 1;
+  let PreviousNum = 0;
+
+  for (let i = 0; i < nthNumber; i++) {
+    //
+    if (i === 0) {
+      fibonacciSequence = 1;
+      continue;
+    }
+    fibonacciSequence = CurrentNum + PreviousNum;
+    PreviousNum = CurrentNum;
+    CurrentNum = fibonacciSequence;
+  }
+  return fibonacciSequence;
 };
 
 // Do not edit below this line
