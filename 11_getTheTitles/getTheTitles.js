a4f64a40c4d57bd3fa172e2689ffe6fe3e0766c3
Cole Harris <cole@dwellstudioco.com>
2022-12-17 23:53:24 -0700

    Solve 11 getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  let titleArray = [];
+  books.forEach((obj) => {
+    for (key in obj) {
+      if (key === "title") {
+        titleArray.push(obj[key]);
+      }
+    }
+  });
+  return titleArray;
 };
 
 // Do not edit below this line
