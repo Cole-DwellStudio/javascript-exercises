ec263b09b527245b39f308e4e9058d493e215b58
Cole Harris <cole@dwellstudioco.com>
2022-12-17 17:49:09 -0700

    Solve 08 calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function (numOne, numTwo) {
+  return numOne + numTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function (numOne, numTwo) {
+  return numOne - numTwo;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+  array.forEach((num) => {
+    sum += num;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let product = parseInt(array[0]);
+  product = parseInt(array[0]);
+  for (let i = 1; i < array.length; i++) {
+    product = array[i] * product;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function (numOne, numTwo) {
+  let result = numOne;
+  for (let i = 1; i < numTwo; i++) {
+    result = numOne * result;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let factorial = num;
+  if (num === 0) {
+    return 1;
+  }
+  for (let i = num - 1; i > 0; i--) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
@@ -29,5 +49,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
